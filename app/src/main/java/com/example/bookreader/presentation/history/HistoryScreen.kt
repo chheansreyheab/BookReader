3d0d7eee39f7341c61b75fdb65587f21c7748fe1
Chhean Sreyheab <chheansreyheab23@gmail.com>
2026-02-12 21:22:08 +0700

    add color

--- app/src/main/java/com/example/bookreader/presentation/history/HistoryScreen.kt
+++ app/src/main/java/com/example/bookreader/presentation/history/HistoryScreen.kt
@@ -1,7 +1,9 @@
 package com.example.bookreader.presentation.history
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,10 +15,12 @@ object HistoryScreen : Screen {
     @Composable
     override fun Content() {
         Box(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background),
             contentAlignment = Alignment.Center
         ) {
-            Text("History Screen")
+            Text("Home Screen")
         }
     }
 }
--- app/src/main/java/com/example/bookreader/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/bookreader/presentation/main/MainActivity.kt
@@ -1,46 +1,34 @@
 package com.example.bookreader.presentation.main
 
+import BookReaderTheme
+import HomeScreen
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.bookreader.R
-import com.example.bookreader.presentation.browse.BrowseScreen
 import com.example.bookreader.presentation.history.HistoryScreen
-import com.example.bookreader.presentation.home.HomeScreen
-import com.example.bookreader.presentation.library.LibraryScreen
 import com.example.bookreader.presentation.navigator.BottomNavItem
 import com.example.bookreader.presentation.setting.SettingScreen
-import com.example.bookreader.ui.theme.BookReaderTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -49,9 +37,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        window.isNavigationBarContrastEnforced = false
+
         setContent {
-            BookReaderTheme {
+            BookReaderTheme(dynamicColor = false) {
                 MainScreen()
             }
         }
@@ -62,7 +50,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-
     val items = listOf(
         BottomNavItem(
             screen = HomeScreen,
@@ -91,10 +78,13 @@ fun MainScreen() {
 
     Scaffold(
         bottomBar = {
-            NavigationBar {
+            NavigationBar(
+                containerColor = MaterialTheme.colorScheme.onError,// Background color of the bar
+                contentColor = MaterialTheme.colorScheme.onSurface// Default color for items
+
+            ) {
                 items.forEach { item ->
                     val isSelected = item == selectedItem
-
                     NavigationBarItem(
                         selected = isSelected,
                         onClick = { selectedItem = item },
@@ -102,14 +92,18 @@ fun MainScreen() {
                         icon = {
                             Icon(
                                 painter = painterResource(
-                                    id = if (isSelected)
-                                        item.selectedIcon
-                                    else
-                                        item.unselectedIcon
+                                    id = if (isSelected) item.selectedIcon else item.unselectedIcon
                                 ),
                                 contentDescription = stringResource(item.tooltip)
                             )
-                        }
+                        },
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = MaterialTheme.colorScheme.primary,
+                            selectedTextColor = MaterialTheme.colorScheme.primary,
+                            unselectedIconColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f),
+                            unselectedTextColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f),
+                            indicatorColor = MaterialTheme.colorScheme.primaryContainer
+                        )
                     )
                 }
             }
@@ -123,32 +117,4 @@ fun MainScreen() {
 
 
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Composable
-fun ScreenContent(text: String, padding: PaddingValues) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(padding),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text)
-    }
-}
-
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BookReaderTheme {
-        Greeting("Android")
-    }
-}
